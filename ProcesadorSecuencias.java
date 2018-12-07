cc24f034c4094ed25cd7c0350bff81599f779735
James Prado <jamesf66888@gmail.com>
2018-12-07 14:30:27 -0600

    UI Version 3.0 con todas las funciones

--- ProcesadorSecuencias.java
+++ ProcesadorSecuencias.java
@@ -62,6 +62,7 @@ public class ProcesadorSecuencias extends Application
 		HBox hbox1 = new HBox(20);
 		HBox hbox2 = new HBox(20);
 		HBox hbox3 = new HBox(20);
+		HBox hbox4 = new HBox(20);
 		Label lbSecuencia1 = new Label("Ingresa la secuencia 1: ");
 		Label lbSecuencia2 = new Label("Ingresa la secuencia 2: ");
 		Label lbDato = new Label("Dato: ");
@@ -76,7 +77,9 @@ public class ProcesadorSecuencias extends Application
 		Button btDesplazar = new Button("Desplazar");
 		Button btReflejar = new Button("Reflejar");
 		Button btDiezmar = new Button("Diezmar");
-		Button btInterpolar = new Button("Interpolar");
+		Button btInterpolacionCero = new Button("Interpolacion Cero");
+		Button btInterpolacionEscalon = new Button("Interpolacion Escalon");
+		Button btInterpolacionLineal = new Button("Interpolacion Lineal");
 		Button btConvolucion = new Button("Convolucion");
 		ToggleGroup tgGroupSecuencia = new ToggleGroup();
 		RadioButton rbSecuencia1 = new RadioButton("Sec 1");
@@ -102,23 +105,25 @@ public class ProcesadorSecuencias extends Application
 	    series2.setName("Secuencia 2");
 	    series3.setName("Secuencia 3");
 
-	    tfSecuencia1.setPrefColumnCount(22);
-	    tfSecuencia2.setPrefColumnCount(22);
+	    tfSecuencia1.setPrefColumnCount(28);
+	    tfSecuencia2.setPrefColumnCount(28);
 	    tfDato.setPrefColumnCount(3);
 
 		procesadorPane.setPadding(new Insets(5));
 		hbox1.setPadding(new Insets(5));
 		hbox2.setPadding(new Insets(5));
 		hbox3.setPadding(new Insets(5));
+		hbox4.setPadding(new Insets(5));
 
 		rbSecuencia1.setToggleGroup(tgGroupSecuencia);
 		rbSecuencia2.setToggleGroup(tgGroupSecuencia);
 		rbSecuencia3.setToggleGroup(tgGroupSecuencia);
 		
 		hbox1.getChildren().addAll(lbSecuencia1, tfSecuencia1, lbSecuencia2, tfSecuencia2, lbDato, tfDato, rbSecuencia1, rbSecuencia2, rbSecuencia3);
-		hbox2.getChildren().addAll(btMostrar, btSumar, btRestar, btMultiplicar, btAmplificar, btDesplazar, btReflejar, btDiezmar, btInterpolar, btConvolucion);
-		hbox3.getChildren().addAll(sc1, sc2, sc3);
-		procesadorPane.getChildren().addAll(hbox1, hbox2, hbox3);
+		hbox2.getChildren().addAll(btMostrar, btSumar, btRestar, btMultiplicar, btAmplificar, btDesplazar, btReflejar, btDiezmar);
+		hbox3.getChildren().addAll(btInterpolacionCero, btInterpolacionEscalon, btInterpolacionLineal, btConvolucion);
+		hbox4.getChildren().addAll(sc1, sc2, sc3);
+		procesadorPane.getChildren().addAll(hbox1, hbox2, hbox3, hbox4);
 
 		Scene scene = new Scene(procesadorPane, 1400, 700);
 		scene.getStylesheets().add("/Chart.css");
@@ -143,12 +148,12 @@ public class ProcesadorSecuencias extends Application
 					{
 						if(i == origen)
 						{
-							x.add(new Par(0, Integer.parseInt(array1[i])));
+							x.add(new Par(0, Double.parseDouble(array1[i])));
 							i++;
 						}
 						else
 						{
-							x.add(new Par(-1, Integer.parseInt(array1[i])));
+							x.add(new Par(-1, Double.parseDouble(array1[i])));
 							i++;
 						}
 					}
@@ -187,12 +192,12 @@ public class ProcesadorSecuencias extends Application
 					{
 						if(i == origen)
 						{
-							y.add(new Par(0, Integer.parseInt(array2[i])));
+							y.add(new Par(0, Double.parseDouble(array2[i])));
 							i++;
 						}
 						else
 						{
-							y.add(new Par(-1, Integer.parseInt(array2[i])));
+							y.add(new Par(-1, Double.parseDouble(array2[i])));
 							i++;
 						}
 					}
@@ -201,10 +206,10 @@ public class ProcesadorSecuencias extends Application
 					{
 						y.get(i).setPos(i - origen);
 					}
-					for(j = origen + 1, i = 1; j < x.size(); j++)
+					for(j = origen + 1, i = 1; j < y.size(); j++)
 					{
 						y.get(j).setPos(i++);
-					}
+					}						
 					//Vaciando el chart
 					if(!sc2.getData().isEmpty())
 					{
@@ -436,6 +441,249 @@ public class ProcesadorSecuencias extends Application
 	            for(int i = 0; i < z.size(); i++) 
 	            {
 	                series3.getData().add(new ScatterChart.Data<>(z.get(i).getPos(), z.get(i).getValor()));
+	            }
+	            sc3.getData().add(series3);
+			}
+		);
+
+		btDiezmar.setOnAction
+		(
+			e -> 
+			{
+				int escalar = Integer.parseInt(tfDato.getText());
+				if(rbSecuencia1.isSelected())
+				{
+					if(!sc1.getData().isEmpty())
+					{
+						sc1.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Diezmacion().diezmar(x, escalar);
+					series1 = new ScatterChart.Series<>();
+	            	series1.setName("Secuencia 1");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series1.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc1.getData().add(series1);
+				}
+				else if(rbSecuencia2.isSelected())
+				{
+					if(!sc2.getData().isEmpty())
+					{
+						sc2.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Diezmacion().diezmar(y, escalar);
+					series2 = new ScatterChart.Series<>(); 
+	            	series2.setName("Secuencia 2");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series2.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc2.getData().add(series2);
+				}
+				else if(rbSecuencia3.isSelected())
+				{
+					if(!sc3.getData().isEmpty())
+					{
+						sc3.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Diezmacion().diezmar(z, escalar);
+					series3 = new ScatterChart.Series<>(); 
+	            	series3.setName("Secuencia 3");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series3.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc3.getData().add(series3);
+				}
+			}
+		);
+
+		btInterpolacionLineal.setOnAction
+		(
+			e -> 
+			{
+				int escalar = Integer.parseInt(tfDato.getText());
+				if(rbSecuencia1.isSelected())
+				{
+					if(!sc1.getData().isEmpty())
+					{
+						sc1.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionLineal(x, escalar);
+					series1 = new ScatterChart.Series<>();
+	            	series1.setName("Secuencia 1");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series1.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc1.getData().add(series1);
+				}
+				else if(rbSecuencia2.isSelected())
+				{
+					if(!sc2.getData().isEmpty())
+					{
+						sc2.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionLineal(y, escalar);
+					series2 = new ScatterChart.Series<>(); 
+	            	series2.setName("Secuencia 2");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series2.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc2.getData().add(series2);
+				}
+				else if(rbSecuencia3.isSelected())
+				{
+					if(!sc3.getData().isEmpty())
+					{
+						sc3.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionLineal(z, escalar);
+					series3 = new ScatterChart.Series<>(); 
+	            	series3.setName("Secuencia 3");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series3.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc3.getData().add(series3);
+				}
+			}
+		);
+
+		btInterpolacionEscalon.setOnAction
+		(
+			e -> 
+			{
+				int escalar = Integer.parseInt(tfDato.getText());
+				if(rbSecuencia1.isSelected())
+				{
+					if(!sc1.getData().isEmpty())
+					{
+						sc1.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionEscalon(x, escalar);
+					series1 = new ScatterChart.Series<>();
+	            	series1.setName("Secuencia 1");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series1.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc1.getData().add(series1);
+				}
+				else if(rbSecuencia2.isSelected())
+				{
+					if(!sc2.getData().isEmpty())
+					{
+						sc2.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionEscalon(y, escalar);
+					series2 = new ScatterChart.Series<>(); 
+	            	series2.setName("Secuencia 2");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series2.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc2.getData().add(series2);
+				}
+				else if(rbSecuencia3.isSelected())
+				{
+					if(!sc3.getData().isEmpty())
+					{
+						sc3.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionEscalon(z, escalar);
+					series3 = new ScatterChart.Series<>(); 
+	            	series3.setName("Secuencia 3");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series3.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc3.getData().add(series3);
+				}
+			}
+		);
+
+		btInterpolacionCero.setOnAction
+		(
+			e -> 
+			{
+				int escalar = Integer.parseInt(tfDato.getText());
+				if(rbSecuencia1.isSelected())
+				{
+					if(!sc1.getData().isEmpty())
+					{
+						sc1.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionCero(x, escalar);
+					series1 = new ScatterChart.Series<>();
+	            	series1.setName("Secuencia 1");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series1.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc1.getData().add(series1);
+				}
+				else if(rbSecuencia2.isSelected())
+				{
+					if(!sc2.getData().isEmpty())
+					{
+						sc2.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionCero(y, escalar);
+					series2 = new ScatterChart.Series<>(); 
+	            	series2.setName("Secuencia 2");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series2.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc2.getData().add(series2);
+				}
+				else if(rbSecuencia3.isSelected())
+				{
+					if(!sc3.getData().isEmpty())
+					{
+						sc3.getData().remove(0);
+					}
+
+					ArrayList<Par> amp = new Interpolacion().interpolacionCero(z, escalar);
+					series3 = new ScatterChart.Series<>(); 
+	            	series3.setName("Secuencia 3");
+		            for(int i = 0; i < amp.size(); i++) 
+		            {
+		                series3.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
+		            }
+		            sc3.getData().add(series3);
+				}
+			}
+		);
+
+		btConvolucion.setOnAction
+		(
+			e -> 
+			{
+				if(!sc3.getData().isEmpty())
+				{
+					sc3.getData().remove(0);
+				}
+				ArrayList<Par> amp = new Convolucion().convolucionar(x, y);
+				series3 = new ScatterChart.Series<>();
+            	series3.setName("Secuencia 1");
+	            for(int i = 0; i < amp.size(); i++) 
+	            {
+	                series3.getData().add(new ScatterChart.Data<>(amp.get(i).getPos(), amp.get(i).getValor()));
 	            }
 	            sc3.getData().add(series3);
 			}
